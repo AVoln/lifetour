f9652a714578ed22cf76e49e57aabe73983049c9
A.Voln <a.voln@list.ru>
2023-09-22 12:58:24 +0300

    pp hero

--- source/js/modules/audio-player.js
+++ source/js/modules/audio-player.js
@@ -10,7 +10,7 @@ const playAudio = () => {
 
     audio.dataset.state = 'ready';
     const src = audio.dataset.src;
-    audio.insertAdjacentHTML('beforeend', '<iframe style="border:none;width:100%;height:100%;" src="' + src + '"></iframe>');
+    audio.insertAdjacentHTML('beforeend', '<iframe style="border:none;border-radius:12px;width:100%;height:100%;" src="' + src + '"></iframe>');
   }, {passive: true});
 };
 
